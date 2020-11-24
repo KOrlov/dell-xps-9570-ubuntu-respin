78c8f7f2afb02c4718f8d1ddbccb1d4c026e6656
korlov@live.ru <korlov@live.ru>
2020-11-24 13:42:40 +0300

    Ubuntu 20.10 version added

--- xps-tweaks.sh
+++ xps-tweaks.sh
@@ -8,7 +8,7 @@ NC='\033[0m' # No Color
 release=$(lsb_release -c -s)
 
 # Check if the script is running under Ubuntu 18.04 Bionic Beaver
-if [ "$release" != "bionic" ] && [ "$release" != "focal" ] ; then
+if [ "$release" != "bionic" ] && [ "$release" != "focal" ] && [ "$release" != "groovy)" ]  ; then
     >&2 echo -e "${RED}This script is made for Ubuntu 18.04/20.04!${NC}"
     exit 1
 fi
